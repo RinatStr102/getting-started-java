f4727948b3483760eb291afdae6b3acfa17d49b9
32Rinat <rinatvaliullinstr102@gmail.com>
2024-10-30 19:06:48 +0300

    добавил все изменения

--- src/test/java/GithubWikiTest.java
+++ src/test/java/GithubWikiTest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class GithubWikiTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubWikiTest {
+    @BeforeAll
+    public static void setUp() {
+       Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void verifySoftAssertionsPageContainsJUnit5Example() {
+
+        Object open;open ("https://github.com");
+
+        $("button.header-search-button").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+        $("#wiki-tab").click();
+    }
 }
