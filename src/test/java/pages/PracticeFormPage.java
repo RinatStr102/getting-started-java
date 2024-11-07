f5315bf7c31e9c1cf76cb91a3553bc9e73ca0819
RinatStr102 <rinatvaliullinstr102@gmail.com>
2024-11-08 00:04:19 +0300

    Добавил начальную реализацию класса PageObject с новой ветке

---
+++ src/test/java/pages/PracticeFormPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PracticeFormPage {
+
+    private SelenideElement firstNameInput = $("#firstName");
+    private SelenideElement lastNameInput = $("#lastName");
+    private SelenideElement emailInput = $("#userEmail");
+}
