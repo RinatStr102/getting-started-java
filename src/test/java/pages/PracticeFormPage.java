a34adc323c3f8d2ea8f6fb26d76bf11908ba0398
RinatStr102 <tamam-102@mail.ru>
2024-11-08 19:31:53 +0300

    Промежуточное сохранение: добавлены основные методы. Осталось еще 4 метода в PracticeFormPage

--- src/test/java/pages/PracticeFormPage.java
+++ src/test/java/pages/PracticeFormPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class PracticeFormPage {
@@ -9,5 +8,69 @@ public class PracticeFormPage {
     private SelenideElement firstNameInput = $("#firstName");
     private SelenideElement lastNameInput = $("#lastName");
     private SelenideElement emailInput = $("#userEmail");
+    private SelenideElement genderRadioButton = $("label[for='gender-radio-3']");
+    private SelenideElement userButtonInput = $("9112233444");
+    private SelenideElement dateOfBirthInput = $("#dateOfBirthInput");
+    private SelenideElement subjectsInput = $("#subjectsContainer");
+    private SelenideElement hobbiesCheckbox = $("label[for='hobbies-checkbox-2']");
+    private SelenideElement uploadPicture = $("#uploadPicture");
+    private SelenideElement currentAddressInput = $("#currentAddress");
+    private SelenideElement stateDropdown = $("#state");
+    private SelenideElement cityDropdown = $("#city");
+    private SelenideElement submitButton = $("#submit");
+
+    public PracticeFormPage setFirstName(String firstName) {
+        firstNameInput.setValue("firstName");
+        return this;
+    }
+
+    public PracticeFormPage setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+        return this;
+    }
+
+    public PracticeFormPage setEmail(String email) {
+        emailInput.setValue(email);
+        return this;
+    }
+
+    public PracticeFormPage selectGender() {
+        genderRadioButton.click();
+        return this;
+    }
+
+    public PracticeFormPage setUserNumber(String userNumber) {
+        userButtonInput.setValue("userNumber");
+        return this;
+    }
+
+    public PracticeFormPage setDateOfBirth(String year, String month, String day) {
+        dateOfBirthInput.click();
+        $(".react-datepicker__year-select").selectOptionByValue(year);
+        $(".react-datepicker__month-select").selectOptionByValue(month);
+        $(".react-datepicker__day--0" + day).click();
+        return this;
+    }
+
+    public PracticeFormPage setSubjects(String subject) {
+        subjectsInput.setValue(subject).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage selectHobby() {
+        hobbiesCheckbox.click();
+        return this;
+    }
+
+    public PracticeFormPage uploadPicture(String picturePath) {
+        uploadPicture.uploadFromClasspath(picturePath);
+        return this;
+    }
+
+    public PracticeFormPage setAddress(String address){
+        currentAddressInput.setValue(address);
+        return this;
+    }
+
 
 }
