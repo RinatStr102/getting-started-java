0c31cb80f19c49343b3633da2751104e0865f090
RinatStr102 <tamam-102@mail.ru>
2024-11-10 14:16:47 +0300

    Вынес все вводимые и проверяемые значения в переменные, добавил генерацию данных с Faker

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
@@ -12,6 +13,18 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestForm {
 
     PracticeFormPage practiceFormPage = new PracticeFormPage();
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String userNumber = faker.phoneNumber().subscriberNumber(10);
+    String address = faker.address().fullAddress();
+    String subject = "Math";
+    String state = "NCR";
+    String city = "Noida";
+
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = "firefox";
@@ -24,22 +37,22 @@ public class TestForm {
     void successForm() {
         open("/automation-practice-form");
         practiceFormPage
-                .setFirstName("Chac")
-                .setLastName("Noris")
-                .setEmail("test@mail.ru")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
                 .selectGender()
-                .setUserNumber("9112233444")
+                .setUserNumber(userNumber)
                 .setDateOfBirth("1965", "11", "24")
-                .setSubjects("Math")
+                .setSubjects(subject)
                 .selectHobby()
                 .uploadPicture("img/Снимок экрана 2024-09-24 в 12.39.43.png")
-                .setAddress("New York")
-                .selectState("NCR")
-                .selectCity("Noida")
+                .setAddress(address)
+                .selectState(state)
+                .selectCity(city)
                 .submitForm();
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Chac"), text("Noris"), text("test@mail.ru"), text("9112233444"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(email), text(userNumber));
     }
 }
